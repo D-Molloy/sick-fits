e796679088627f39dcc1065eb4ff06445d2d48a9
Denis Molloy <denis.molloy@gmail.com>
2019-06-30 23:43:22 -0700

    updated prisma schema and added createItem mutation and items query

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,11 +1,31 @@
 const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    // create a dog
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if the user is logged in
+
+    // Our API for the prisma db is defined in prisma.graphql under `type Mutations`
+    // ctx has a db property due to our exposing of the db to the context in createServer.js
+    // info param contains the query
+    // ctx.db.mutation.createItem returns a Promise, so we have to make it async to save the actual value into the item
+    // alternative would be to just return the item: `return ctx.db.mutation.createItem`, but the below syntax is better for debugging
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          // we could manually place everything from the args in this object (name, image, etc)
+          ...args
+        }
+      },
+      info
+    );
+    // console.log(item)
+    return item;
   }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
 };
 
 module.exports = mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,23 @@
+/**
+ *  - TO ADD A NEW TYPE
+ *    * add it to the data model (schema.graphql)
+ *    * run `npm run deploy` to push the changes up to prisma
+ *       * a new copy of prisma.graphql is brought down (all queries/mutations)
+ *    * able to add mutations and queries
+ *    * can write resolvers in Mutation.js and Query.js
+ */
+
+// all prisma-built crud apis are found in prisma.graphql => type Query
+
+// if our query is the exact same as the query in prisma.graphql we can forward it to prisma directly
+const { forwardTo } = require("prisma-binding");
 const Query = {
-  dogs(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    return global.dogs;
-  }
+  items: forwardTo("db")
+  // NOT USING FORWARDTO:
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
 };
 
 module.exports = Query;
