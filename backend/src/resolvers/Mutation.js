885736d48b0ed521575fe04c29e0cb6032b4e087
Denis Molloy <denis.molloy@gmail.com>
2019-10-22 23:45:49 -0700

    added signup mutation and resolver, implemented cookies and JWT

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if the user is logged in
@@ -45,6 +48,33 @@ const mutations = {
     // TODO:
     // 3 - delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase the new email
+    args.email = args.email.toLowerCase();
+    // hash the password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+    // create JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set the JWT as a cookie on the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      // cookie dies in a year
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    // return the user to the browser
+    return user;
   }
 };
 // createDog(parent, args, ctx, info) {
