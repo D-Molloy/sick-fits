77052e119ff257e251f4f20507d2ec6520a2cb5f
Denis Molloy <denis.molloy@gmail.com>
2019-11-09 23:13:29 -0800

    added permission checkboxes into state

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f95ca8c8d59f3f149576";
+/******/ 	var hotCurrentHash = "196a20135fd7c8e42687";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
