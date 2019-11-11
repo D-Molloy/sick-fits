25768d4194b856b0237a3f05d937f9c383571ad6
Denis Molloy <denis.molloy@gmail.com>
2019-11-11 11:41:17 -0800

    setup addToCart button

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c5cb9339c431e652a5ba";
+/******/ 	var hotCurrentHash = "fbba54df6590bbcccca9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
