9033d4c409843a01170adc2238d7086752121708
Denis Molloy <denis.molloy@gmail.com>
2019-11-11 22:09:57 -0800

    started search component

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -36,7 +36,12 @@ class CreateItem extends Component {
   };
   handleChange = e => {
     const { name, type, value } = e.target;
+
     const val = type === "number" ? parseFloat(value) : value;
+    if (val > 99999999) {
+      this.setState({ price: 0 });
+      alert("Maximum price is $999,999.99");
+    }
     this.setState({ [name]: val });
   };
 
@@ -55,7 +60,6 @@ class CreateItem extends Component {
       }
     );
     const file = await res.json();
-    console.log(file);
     this.setState({
       image: file.secure_url,
       largeImage: file.eager[0].secure_url
