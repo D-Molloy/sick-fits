9adf46dc3a36f1f8e67c54d75274bb13a5636c41
Denis Molloy <denis.molloy@gmail.com>
2019-11-16 13:41:20 -0800

    Orders page displaying all orders

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "217c752a7097490bed69";
+/******/ 	var hotCurrentHash = "47acadb7f04a013eedf4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/lib/formatMoney.js
+++ frontend/lib/formatMoney.js
@@ -1,11 +1,12 @@
 export default function(amount) {
   const options = {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
   };
+  console.log("Amount from formatMoney", amount);
   // if its a whole, dollar amount, leave off the .00
   if (amount % 100 === 0) options.minimumFractionDigits = 0;
-  const formatter = new Intl.NumberFormat('en-US', options);
+  const formatter = new Intl.NumberFormat("en-US", options);
   return formatter.format(amount / 100);
 }
